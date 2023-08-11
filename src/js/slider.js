4589c889db00f1a4d52d40225c2676409957fbd2
iraand <iraand@bigmir.net>
2023-08-11 12:32:37 +0300

    added active class to slider navigation

--- src/js/slider.js
+++ src/js/slider.js
@@ -7,16 +7,31 @@ export const slider = () => {
     const buttonNextEl = document.querySelector(".button__next");
     const buttonPrevEl = document.querySelector(".button__prev");
 
-    const setActiveSlid = () => {
-     console.log(swiper.activeIndex);
+    const setActiveButton = (index) => {
+        buttonNextEl.classList.add("active");
+        buttonPrevEl.classList.add("active");
 
+        console.log(Number(index));
+
+        if (Number(index) === 0) {
+         buttonPrevEl.classList.remove("active");
+        }
+        if (Number(index) === 2) {
+         buttonNextEl.classList.remove("active");
+        } 
+               
+    }
+
+    const setActiveSlid = () => {
         const slideNumber = document.querySelector(".slide-number");
         let span = `0${swiper.activeIndex + 1}`;
         slideNumber.innerHTML = span;
 
+        setActiveButton(swiper.activeIndex);
+
      return swiper.activeIndex + 1;
     };
-
+    setActiveButton(0)
     setActiveSlid();
 
     buttonNextEl.addEventListener("click", () => {
